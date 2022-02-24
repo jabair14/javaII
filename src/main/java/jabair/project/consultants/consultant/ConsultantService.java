07a06debc00625330e9f378accd59384049b8aac
Jabair <jabairk@gmail.com>
2022-02-24 16:15:15 -0500

    fixed repo

--- src/main/java/jabair/project/consultants/consultant/ConsultantService.java
+++ src/main/java/jabair/project/consultants/consultant/ConsultantService.java
@@ -1,19 +1,21 @@
 package jabair.project.consultants.consultant;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ConsultantService {
+
+    private final ConsultantRepository consultantRepository;
+
+    @Autowired
+
+    public ConsultantService(ConsultantRepository consultantRepository){
+        this.consultantRepository = consultantRepository;
+    }
     public List<Consultant> getConsultants(){
-        return List.of(
-                new Consultant(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        "Philadelphia"
-                )
-        );
+        return consultantRepository.findAll();
     }
 }
